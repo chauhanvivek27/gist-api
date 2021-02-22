6d8997b085638648cc35537706281d986ab23488
vivek chauhan <vivekchauhan@viveks-MacBook-Pro.local>
2021-02-22 23:45:53 +0400

    add badge

--- src/gistcard/index.js
+++ src/gistcard/index.js
@@ -1,15 +1,25 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import './styles.css';
+import Badge from '../badge';
 
 export default class GistCard extends PureComponent {
     render() {
         const {gistData} = this.props;
         const noOfFiles = Object.keys(gistData.files).length;
+        const arr = [];
+        for(let file in gistData.files) {
+            let language = gistData.files[file].language;
+            //remove duplicate file types
+            if(arr.indexOf(language) === -1) {
+                arr.push(language);
+            }
+        }
         return (
                 <div className="card">
                     <p className="lead">{(gistData.description) || 'No Description'}</p>
                     <p className="text-secondary">{noOfFiles} {(noOfFiles > 1) ? 'Files' : 'File'}</p>
+                    <Badge files={arr}/>
                 </div>
         );
     }
